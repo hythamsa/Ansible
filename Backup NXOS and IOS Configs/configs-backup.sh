974645264dd2e612914b2175a03b3f9fd26bdead
Hytham Safieh <hytham.a@gmail.com>
2018-02-19 02:12:03 -0600

    Create configs-backup.sh

---
+++ Backup NXOS and IOS Configs/configs-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Run Ansbile playbook backup.yml to backup configurations of all NXOS and IOS devices. Once completed, create a directory and move all files into it
+
+basedir=/etc/ansible/backup
+tdate=`date +%Y_%m_%d`
+
+# Run Ansible backup
+/usr/bin/ansible-playbook /etc/ansible/backup.yml
+
+# Move files into today's date
+cd $basedir
+
+if [ ! -d $tdate ]
+        then
+                mkdir `date +%Y_%m_%d`
+fi
+
+mv $basedir/*_config* $basedir/$tdate
